fc4912ad3805242672ea4c9c3eaa872059e1ecd8
wy6714 <78013025+wy6714@users.noreply.github.com>
2024-05-21 10:51:35 -0400

    edit event and time done

--- Juneteenth/Assets/_Script/SyncTextPosition.cs
+++ Juneteenth/Assets/_Script/SyncTextPosition.cs
@@ -5,28 +5,25 @@ using TMPro;
 
 public class SyncTextPosition : MonoBehaviour
 {
-    public Transform objectToFollow; // The object in the scene to follow
-    public RectTransform uiTextTransform; // The RectTransform of the UI Text (TextMeshPro)
-    private Camera mainCamera; // Reference to the main camera
-
+    public Transform objectToFollow;
+    public RectTransform uiTextTransform;
+    private Camera mainCamera;
     void Start()
     {
-        // Find and assign the main camera at runtime
         mainCamera = Camera.main;
     }
     void Update()
     {
-        // Check if mainCamera is assigned
         if (mainCamera == null)
         {
             Debug.LogWarning("Main Camera not found");
             return;
         }
 
-        // Convert the world position of the circle to screen position
+        // circle world position -> screen position
         Vector3 screenPosition = mainCamera.WorldToScreenPoint(objectToFollow.position);
 
-        // Convert the screen position to UI position
+        //  screen position -> UI position
         RectTransformUtility.ScreenPointToLocalPointInRectangle(uiTextTransform.parent as RectTransform, screenPosition, mainCamera, out Vector2 localPoint);
 
         // Update the UI Text's position
